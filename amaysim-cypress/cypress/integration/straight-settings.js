47e4eb174b48a14781988a59bd322563a391655c
Tyke Tardecilla <ttardecilla@CUP-MNL-M792.local>
2021-11-08 00:19:03 +0800

    test update

--- amaysim-cypress/cypress/integration/straight-settings.js
+++ amaysim-cypress/cypress/integration/straight-settings.js
@@ -27,12 +27,12 @@ it('Log in to My Amaysim', () => {
 // it('Change Call Forwarding Number', () =>{
     cy.get('#edit_settings_call_forwarding').click()
     cy.get('.confirm_popup_confirm', {timeout: 60000}).click()
-    cy.get('#my_amaysim2_setting_call_divert_number').clear().type('0412345678{enter}')
+    cy.get('#my_amaysim2_setting_call_divert_number').clear().type('0212340000{enter}')
     //cy.get('#update_call_forwarding_form > div.row.border-top.padding-top.margin-top > div > input', {timeout: 60000}).click()
     cy.contains('We have successfully updated your SIM settings.', {timeout: 60000})
     cy.get('.form_info_popup > :nth-child(4)', {timeout: 60000}).click()
     //cy.get('#body-content > div.form_info_popup.reveal-modal.padding-none.open > a', {timeout: 60000}).click
     // cy.visit('https://www.amaysim.com.au/my-account/my-amaysim/settings')
     // cy.location('pathname', {timeout: 10000}).should('include', '/my-account/my-amaysim/settings')
-    cy.contains('Forward calls to 0412345678')
+    cy.contains('Forward calls to 0212340000')
 })
